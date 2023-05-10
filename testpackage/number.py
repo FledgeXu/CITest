91c3fbe27a0a64442daa59aa5f99f4e8413c3537
Fledge Shiu <xzk0701@gmail.com>
2023-05-10 12:47:03 -0400

    Add files.

---
+++ testpackage/number.py
@@ -0,0 +1,6 @@
+def square(x: float):
+    if x%2 == 0:
+        return x*x
+    else:
+        return x+2
+    
---
+++ tests/test_main.py
@@ -0,0 +1,4 @@
+from testpackage.number import square
+
+def test_square():
+    assert square(2) == 4
